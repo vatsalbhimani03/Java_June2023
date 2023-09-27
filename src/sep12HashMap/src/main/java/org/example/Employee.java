4125b4fb0ebef4a448e3452ee11a0f7536464e29
Vatsal Bhimani <vatsal.bhimani@georgebrown.ca>
2023-09-26 22:28:23 -0400

    sep12HashMap Project is added

---
+++ src/sep12HashMap/src/main/java/org/example/Employee.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+@Getter
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double salary;
+}
