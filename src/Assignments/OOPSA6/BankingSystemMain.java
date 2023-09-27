870a2e1f3e3d70a0ddb1ce4e1b1a67b921d48026
Vatsal Bhimani <vatsal.bhimani@georgebrown.ca>
2023-09-26 22:27:19 -0400

    June2023 Project is added

---
+++ src/Assignments/OOPSA6/BankingSystemMain.java
@@ -0,0 +1,2 @@
+package Assignments.OOPSA6;public class BankingSystemMain {
+}
---
+++ src/mapJune2023/src/main/java/org/example/Employee.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private double salary;
+}
